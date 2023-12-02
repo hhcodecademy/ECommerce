a64809f45f6562037dc76556d27edb7b50553b49
Huseyn <h.hasanli@pa.gov.az>
2023-12-02 12:24:09 +0400

    Update Web UI

---
+++ ECommerce.WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Areas.Admin.Controllers
+{
+
+    [Area("admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
